65536d0a36cc1734b852fe3a4ae9b69040ecac3e
Lauraems <bertille-laura.emah-belinga@informatik.hs-fulda.de>
2022-01-29 20:51:05 +0100

    wk-dev: ajout d'un commentaire

--- src/b_deuxieme_session_23_01_2022/Execice_niveau3_gestion_note_maths.java
+++ src/b_deuxieme_session_23_01_2022/Execice_niveau3_gestion_note_maths.java
@@ -7,6 +7,7 @@ public class Execice_niveau3_gestion_note_maths {
     public static void main(String[] args) {
         HashMap<String,Double> schule = new HashMap<>();
 // noms et le commentaire de willy
+        //Laura a aussi ajoute un commentaire
         schule.put("Wilfried",new Double(17));
         schule.put("Jacque",new Double(14));
         schule.put("Nelly",new Double(15));

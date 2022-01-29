9d0ca23a74be990af809559c3bf3ea0439a318ff
WilfriedNono90 <wilfried.nono90@gmail.com>
2022-01-29 13:23:40 +0100

    wk-dev: review kommentar

--- src/b_deuxieme_session_23_01_2022/Execice_niveau3_gestion_note_maths.java
+++ src/b_deuxieme_session_23_01_2022/Execice_niveau3_gestion_note_maths.java
@@ -7,6 +7,8 @@ public class Execice_niveau3_gestion_note_maths {
     public static void main(String[] args) {
         HashMap<String,Double> schule = new HashMap<>();
 // noms et le commentaire de willy
+
+        // REVIEW_KOMMENTAR wk-dev change name and
         schule.put("Wilfried",new Double(17));
         schule.put("Jacque",new Double(14));
         schule.put("Nelly",new Double(15));

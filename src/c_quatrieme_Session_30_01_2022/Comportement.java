6ee8da3842acccd939338821c95c1f43ee7f9b18
WilfriedNono90 <wilfried.nono90@gmail.com>
2022-02-05 12:35:03 +0100

    wk-dev: Session 5 05.02.2022

--- src/c_quatrieme_Session_30_01_2022/Comportement.java
+++ src/c_quatrieme_Session_30_01_2022/Comportement.java
@@ -2,7 +2,7 @@ package c_quatrieme_Session_30_01_2022;
 
 public interface Comportement {
 
-    public void macher();
+    public void macher(int numerodeCarte);
     public void sauter();
     public void attaquer();
 

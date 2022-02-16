74d38335cec245f913bd7954fa3abc341ef6aecb
Jacques Priso <jacques.jackeve.priso@mni.thm.de>
2022-02-16 20:51:04 +0100

    wk-dev-feature WD-22-jacques

--- src/wk_dev_workshop/userService/Client.java
+++ src/wk_dev_workshop/userService/Client.java
@@ -2,7 +2,7 @@ package wk_dev_workshop.userService;
 
 //WD-1 : le nom inter n'Est pas parlant, il ne respecte pas le cleanCode
 //WD-1 : enlever l'implementation on l'utilisera ailleur
-public class Client extends User implements Inter {
+public class Client extends User implements activites {
     public Client(String nom, String prenom, String email, String ville, int age, String adresse1, int plz) {
         super(nom, prenom, email, ville, age, adresse1, plz);
     }
@@ -11,7 +11,7 @@ public class Client extends User implements Inter {
     public static void main(String[] args) {
         Client client = new Client("Priso", "Jacques", "jackevepriso@yahoo.fr", "Linden",
                 26, "Bahnhofstrasse 25", 35440);
-        System.out.println(client.email + " "+ client.ville);
+        System.out.println(client.getEmail() + " "+ client.getVille());
 
     }
 
--- src/wk_dev_workshop/userService/activites.java
+++ src/wk_dev_workshop/userService/activites.java
@@ -1,6 +1,6 @@
 package wk_dev_workshop.userService;
 // WD-1 :  change name. utiliser un meilleur nom
-public interface Inter {
+public interface activites {
 
     public void bestellen();
     public void stoernieren();
